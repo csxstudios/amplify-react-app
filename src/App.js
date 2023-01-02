e5031894ced6f354edde5e658f1a00aa89d1cc44
csxstudios <csxstudios@gmail.com>
2023-01-02 10:23:44 -0500

    Add basic frontend auth/login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+  console.log(user);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,4 +30,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
