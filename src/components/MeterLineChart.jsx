8b81533b7bb99760a1f75cc9be6fe74058f8376f
csxstudios <csxstudios@gmail.com>
2023-01-16 20:02:36 -0500

    Add devextreme temperature line chart

--- src/components/MeterLineChart.jsx
+++ src/components/MeterLineChart.jsx
@@ -20,34 +20,66 @@ const MeterLineChart = (props) => {
     }
 
     return (
-        <Chart
-            palette="Violet"
-            dataSource={props.chartData}
-        >
-            <Series
-                valueField="meter"
-                argumentField="dateISO"
-                name="user"
-                type="line"
-                color="#ffaa66" />
-            <Margin bottom={20} />
-            <ArgumentAxis
-                valueMarginsEnabled={false}
-                discreteAxisDivisionMode="crossLabels"
+        <>
+            <Chart
+                palette="Violet"
+                dataSource={props.chartData}
             >
-                <Label customizeText={customizeLabel} />
-            </ArgumentAxis>
-            <Legend
-                verticalAlignment="bottom"
-                horizontalAlignment="center"
-                itemTextPosition="bottom"
-            />
-            <Export enabled={true} />
-            <Title text="Energy Usage">
-                <Subtitle text="As of 1/16/2023" />
-            </Title>
-            <Tooltip enabled={true} />
-        </Chart>
+                <Series
+                    valueField="meter"
+                    argumentField="dateISO"
+                    name="user"
+                    type="line"
+                    color="#ffaa66" />
+                <Margin bottom={20} />
+                <ArgumentAxis
+                    valueMarginsEnabled={false}
+                    discreteAxisDivisionMode="crossLabels"
+                >
+                    <Label customizeText={customizeLabel} />
+                </ArgumentAxis>
+                <Legend
+                    verticalAlignment="bottom"
+                    horizontalAlignment="center"
+                    itemTextPosition="bottom"
+                    visible={false}
+                />
+                <Export enabled={true} />
+                <Title text="Energy Usage">
+                    {/* <Subtitle text="As of 1/16/2023" /> */}
+                </Title>
+                <Tooltip enabled={true} />
+            </Chart>
+            <Chart
+                palette="Violet"
+                dataSource={props.chartData}
+            >
+                <Series
+                    valueField="temp"
+                    argumentField="dateISO"
+                    name="user"
+                    type="line"
+                    color="#0d6efd" />
+                <Margin bottom={20} />
+                <ArgumentAxis
+                    valueMarginsEnabled={false}
+                    discreteAxisDivisionMode="crossLabels"
+                >
+                    <Label customizeText={customizeLabel} />
+                </ArgumentAxis>
+                <Legend
+                    verticalAlignment="bottom"
+                    horizontalAlignment="center"
+                    itemTextPosition="bottom"
+                    visible={false}
+                />
+                <Export enabled={true} />
+                <Title text="Temperature">
+                    {/* <Subtitle text="As of 1/16/2023" /> */}
+                </Title>
+                <Tooltip enabled={true} />
+            </Chart>
+        </>
     )
 }
 
